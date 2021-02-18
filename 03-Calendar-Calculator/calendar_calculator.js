a7a08a4ee00b90e88bd3888abd563ce13759dcb4
ahlam alshaba <ama1977ln@outlook.com>
2021-02-18 17:36:08 +0100

    calcoulet days

--- 03-Calendar-Calculator/calendar_calculator.js
+++ 03-Calendar-Calculator/calendar_calculator.js
@@ -1,6 +1,28 @@
 //insert your pseudocode below
-/*
+/* we can make 2 var as month and days.
+for the var month will contin the name of the months.
+for the second var will contin the num of day of the month 
+
 here
 */
 
 //Insert your code below
+var month, day, Result;
+
+
+month = 'February';
+day = 18;
+
+if (month == 'January') {
+  Result = 0 + day;
+}
+if (month == 'February') {
+  Result = 31 + day;
+}
+if (month == 'March') {
+  Result = 59 + day;
+}
+if (month == 'April') {
+  Result = 90 + day;
+}
+window.alert(Result);
