4eeba8800fe333106bed1e036db77cb27b9fc37e
ahlam alshaba <ama1977ln@outlook.com>
2021-02-18 12:29:27 +0100

    level 10

--- 01-Blockly/blockly.js
+++ 01-Blockly/blockly.js
@@ -1,1 +1,30 @@
 //Insert level 9 code below
+while (notDone()) {
+        if (isPathForward()) {
+          moveForward();
+        } else {
+          if (isPathLeft()) {
+            turnLeft();
+          }
+          moveForward();
+        }
+      }
+    
+    
+      while (notDone()) {
+        moveForward();
+        if (isPathForward()) {
+          if (isPathRight()) {
+            turnRight();
+          } else {
+            if (isPathLeft()) {
+              turnLeft();
+            }
+          }
+        } else {
+          if (isPathLeft()) {
+            turnLeft();
+          } else {
+            turnRight();
+          }
+        
