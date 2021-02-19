eaa145189f72df0068a8d728f16923caf286fc3b
ahlam alshaba <ama1977ln@outlook.com>
2021-02-19 16:58:07 +0100

    canceled the code from password generator

--- 04-Password-Generator/password_generator.js
+++ 04-Password-Generator/password_generator.js
@@ -7,12 +7,3 @@ the 1 var will be characters , the 2 var will be num , the 3 var will be sym.
 */
 
 //insert your code below
-function generatePassword() {
-    var length = 8,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-        retVal = "";
-    for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
-    }
-    return retVal;
-}
