97e506be45faabce01b082ed9463c8878128d847
ahlam alshaba <ama1977ln@outlook.com>
2021-02-19 16:05:02 +0100

    added pseudocode

--- 04-Password-Generator/password_generator.js
+++ 04-Password-Generator/password_generator.js
@@ -1,8 +1,9 @@
 //insert your pseudocode below
-/*here we have 3 var to work with.
-the 1 var will be characters , the 2 var will be num , the 3 var will be sym.
+/* we need to creat a random password generator with a lest 8 characters and 1 digit, one special symbol. 
 
 here
+here we have 3 var to work with.
+the 1 var will be characters , the 2 var will be num , the 3 var will be sym.
 */
 
 //insert your code below
